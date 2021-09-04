dd24ef4d2ff96f31ca8a8a6dee514b8e4b507441
LOKESHGANDHI19 <89632377+LOKESHGANDHI19@users.noreply.github.com>
2021-09-04 17:50:12 +0530

    Create 6. creating multiple objects by one type only.java

---
+++ 6. creating multiple objects by one type only.java
@@ -0,0 +1,12 @@
+public class Calculation{  
+	  void fact(int  n){  
+	  int fact=1;  
+	  for(int i=1;i<=n;i++){  
+	   fact=fact*i;  
+	  }  
+	 System.out.println("factorial is "+fact);  
+	}  
+    public static void main(String args[]) {
+    new Calculation().fact(5);
+    }
+}
