5c051813f9887ae17dc972f33fad9a25b1dc97b3
LOKESHGANDHI19 <89632377+LOKESHGANDHI19@users.noreply.github.com>
2021-09-04 18:21:23 +0530

    Create 11 java default constructor.java

---
+++ 11 java default constructor.java
@@ -0,0 +1,9 @@
+public class Bike1{  
+	Bike1(){System.out.println("Bike is created");}  
+
+
+    public static void main(String args[]) {
+     	Bike1 b=new Bike1();  
+    }
+}
+© 2021 GitHub, Inc.
