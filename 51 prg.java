e8c3d3b115a69734fe5ea96ac9d632043989a692
LOKESHGANDHI19 <89632377+LOKESHGANDHI19@users.noreply.github.com>
2021-09-24 16:16:42 +0530

    Create 51 prg.java

---
+++ 51 prg.java
@@ -0,0 +1,6 @@
+public	class A{  
+	public static void main(String args[]){  
+	StringBuilder sb=new StringBuilder("Hello");  
+	sb.replace(1,3,"Java");  
+	System.out.println(sb);//prints HJavalo  
+	}  
