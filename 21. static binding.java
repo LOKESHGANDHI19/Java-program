4b469a625278a5dd92e7bd2ebd3d18376f9c5dc3
LOKESHGANDHI19 <89632377+LOKESHGANDHI19@users.noreply.github.com>
2021-09-24 15:01:02 +0530

    Create 21. static binding.java

---
+++ 21. static binding.java
@@ -0,0 +1,8 @@
+public	class Dog{  
+	 private void eat(){System.out.println("dog is eating...");}  
+	  
+	 public static void main(String args[]){  
+	  Dog d1=new Dog();  
+	  d1.eat();  
+	 }  
+	}  
